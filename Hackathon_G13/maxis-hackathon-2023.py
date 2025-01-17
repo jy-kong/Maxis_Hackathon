1778accbd1b3457e4e96069088601e1a6f96ef5d
jy-kong <73959291+jy-kong@users.noreply.github.com>
2025-01-17 22:07:00 +0800

    Update maxis-hackathon-2023.py

--- Hackathon_G13/maxis-hackathon-2023.py
+++ Hackathon_G13/maxis-hackathon-2023.py
@@ -31,7 +31,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("Hackathon_G13/style/style.css")
 
 
 def display_dataset():
